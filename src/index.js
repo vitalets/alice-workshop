34c1b86ef4d336c812ffcbf75b0d76e93ef3acc2
vitalets <vitalets@yandex-team.ru>
2019-11-15 00:15:50 +0600

    step 6.1 - check on real device

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const micro = require('micro');
 
 module.exports = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://alice-dev.vitalets.xyz');
   const {request, session, version} = await micro.json(req);
   const response = {
     text: session.new ? 'Добро пожаловать' : `Вы сказали: ${request.command}`,
