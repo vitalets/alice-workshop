a07b61d5e1b0809bc1ee7d39acc188d6855a2c98
vitalets <vitalets@yandex-team.ru>
2020-05-19 20:01:52 +0300

    step15: many users

--- src/index.js
+++ src/index.js
@@ -2,52 +2,55 @@ const micro = require('micro');
 const replies = require('./replies');
 
 /**
- * @type {{number1: number, number2: number}}
+ * @type {Map<{number1: number, number2: number}>}
  */
-let question;
+const questions = new Map();
 
 const server = micro(async (req, res) => {
   const { request, session } = await micro.json(req);
   const response = session.new
     ? replies.welcome()
-    : checkAnswer(request.command);
+    : checkAnswer(session.session_id, request.command);
   return {
     response,
     version: '1.0'
   };
 });
 
-function checkAnswer(command) {
+function checkAnswer(sessionId, command) {
+  let question = questions.get(sessionId);
   if (!question) {
-    question = generateQuestion();
+    question = generateQuestion(sessionId);
     return replies.firstQuestion(question);
   }
 
-  if (isCorrectAnswer(command)) {
-    question = generateQuestion();
+  if (isCorrectAnswer(question, command)) {
+    question = generateQuestion(sessionId);
     return replies.correctAnswer(question);
   }
 
   if (/сдаюсь/i.test(command)) {
     const answer = question.number1 + question.number2;
-    question = generateQuestion();
+    question = generateQuestion(sessionId);
     return replies.capitulate(answer, question);
   }
 
   return replies.incorrectAnswer(question);
 }
 
-function isCorrectAnswer(command) {
+function isCorrectAnswer(question, command) {
   const matches = command.match(/[0-9]+/);
   const correctAnswer = question.number1 + question.number2;
   return matches && Number(matches[0]) === correctAnswer;
 }
 
-function generateQuestion() {
-  return {
+function generateQuestion(sessionId) {
+  const question = {
     number1: Math.ceil(Math.random() * 20),
     number2: Math.ceil(Math.random() * 20),
   };
+  questions.set(sessionId, question);
+  return question;
 }
 
 const PORT = 3000;
