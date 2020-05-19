10d0a15b7735327a8941bb5bd0cee8903622294f
vitalets <vitalets@yandex-team.ru>
2020-05-19 20:33:05 +0300

    step15: use session state

--- src/index.js
+++ src/index.js
@@ -1,58 +1,58 @@
 const micro = require('micro');
 const replies = require('./replies');
 
-/**
- * @type {{number1: number, number2: number}}
- */
-let question;
-
 const server = micro(async (req, res) => {
   if (req.method !== 'POST') {
     return 'Server is running';
   }
 
-  const { request, session } = await micro.json(req);
+  const { request, session, state } = await micro.json(req);
+  const sessionState = state && state.session || {};
   const response = session.new
     ? replies.welcome()
-    : checkAnswer(request.command);
+    : checkAnswer(sessionState, request.command);
 
   return {
     response,
+    session_state: sessionState,
     version: '1.0'
   };
 });
 
-function checkAnswer(command) {
+function checkAnswer(sessionState, command) {
+  let question = sessionState.question;
   if (!question) {
-    question = generateQuestion();
+    question = generateQuestion(sessionState);
     return replies.firstQuestion(question);
   }
 
-  if (isCorrectAnswer(command)) {
-    question = generateQuestion();
+  if (isCorrectAnswer(question, command)) {
+    question = generateQuestion(sessionState);
     return replies.correctAnswer(question);
   }
 
   if (/сдаюсь/i.test(command)) {
     const answer = question.number1 + question.number2;
-    question = generateQuestion();
+    question = generateQuestion(sessionState);
     return replies.capitulate(answer, question);
   }
 
   return replies.incorrectAnswer(question);
 }
 
-function isCorrectAnswer(command) {
+function isCorrectAnswer(question, command) {
   const matches = command.match(/[0-9]+/);
   const correctAnswer = question.number1 + question.number2;
   return matches && Number(matches[0]) === correctAnswer;
 }
 
-function generateQuestion() {
-  return {
+function generateQuestion(sessionState) {
+  const question = {
     number1: Math.ceil(Math.random() * 20),
     number2: Math.ceil(Math.random() * 20),
   };
+  sessionState.question = question;
+  return question;
 }
 
 const PORT = 3000;
