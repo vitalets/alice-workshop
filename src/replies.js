5ec9cb2f00c2af532f53d3dba9c35d451f7e4061
vitalets <vitalets@yandex-team.ru>
2020-05-19 14:00:54 +0300

    step10: introduce math teacher

--- src/replies.js
+++ src/replies.js
@@ -3,24 +3,53 @@
  */
 exports.welcome = () => {
   return {
-    text: 'Привет',
-    tts: '<speaker audio="alice-music-harp-1.opus">Привет, я ваш голосовой помощник',
+    text: 'Привет. Я ваш новый учитель математики. Начинаем урок?',
+    tts: '<speaker audio="alice-music-harp-1.opus">Привет. Я ваш новый учитель математики. Начинаем урок?',
     buttons: [
-      { title: 'Здравствуй', hide: true },
-      { title: 'Как дела?', hide: true },
+      { title: 'Начинаем', hide: true },
     ],
     end_session: false
   };
 };
 
 /**
- * Повторяем фразу пользователя.
+ * Первый вопрос пользователю.
  *
- * @param {String} command
+ * @param {Number} number1
+ * @param {Number} number2
  */
-exports.repeatUserCommand = command => {
+exports.firstQuestion = ({ number1, number2 }) => {
   return {
-    text: `Вы сказали: ${command}`,
+    text: `Сколько будет ${number1} + ${number2} = ?`,
+    tts: `Сколько будет ${number1} + ${number2}`,
+    end_session: false
+  };
+};
+
+/**
+ * Реакция на неправильный ответ.
+ *
+ * @param {Number} number1
+ * @param {Number} number2
+ */
+exports.incorrectAnswer = ({ number1, number2 }) => {
+  return {
+    text: `Неверно. Попробуй еще раз: ${number1} + ${number2} = ?`,
+    tts: `Неверно. Попробуй еще раз: ${number1} + ${number2}`,
+    end_session: false
+  };
+};
+
+/**
+ * Реакция на правильный ответ.
+ *
+ * @param {Number} number1
+ * @param {Number} number2
+ */
+exports.correctAnswer = ({ number1, number2 }) => {
+  return {
+    text: `Правильно! Следующий вопрос: ${number1} + ${number2} = ?`,
+    tts: `Правильно! Следующий вопрос: ${number1} + ${number2}`,
     end_session: false
   };
 };
